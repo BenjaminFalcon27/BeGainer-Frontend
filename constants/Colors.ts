03bf710a51c0a49ee63f017f65c2b792452323d7
benjamin.trolais <benjamin.trolais@octopuslab.fr>
2025-03-31 10:56:19 +0200

    💄 Edit themes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,29 @@
 /**
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ * For customizations, you can adjust these values based on your requirements.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#59388A'; // Couleur primaire pour le mode clair
+const tintColorDark = '#59388A';  // Couleur primaire pour le mode sombre
+const backgroundDark = '#1A141F'; // Fond par défaut du mode sombre
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: '#11181C', // Texte par défaut pour le mode clair
+    background: '#fff', // Fond par défaut pour le mode clair
+    primary: '#59388A', // Couleur primaire en mode clair
+    tint: tintColorLight, // Couleur pour les icônes ou autres éléments interactifs en mode clair
+    icon: '#687076', // Couleur des icônes par défaut
+    tabIconDefault: '#687076', // Icônes par défaut dans les tabs
+    tabIconSelected: tintColorLight, // Icônes sélectionnées dans les tabs
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: '#fff', // Texte en blanc pour le mode sombre
+    background: backgroundDark, // Fond personnalisé en mode sombre
+    primary: '#59388A', // Couleur primaire en mode sombre
+    tint: tintColorDark, // Couleur pour les icônes ou autres éléments interactifs en mode sombre
+    icon: '#9BA1A6', // Couleur des icônes en mode sombre
+    tabIconDefault: '#9BA1A6', // Icônes par défaut dans les tabs en mode sombre
+    tabIconSelected: tintColorDark, // Icônes sélectionnées dans les tabs en mode sombre
   },
 };
